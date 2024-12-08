b3b282178251d62d760df7bc59dec863ba258036
Boualam3 <boualam.bourahla@gmail.com>
2024-12-09 00:59:24 +0100

    correct db name identifier

--- HubspotApiIntegration/settings/prod.py
+++ HubspotApiIntegration/settings/prod.py
@@ -4,5 +4,5 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default="postgresql://postgres:postgres@localhost:5432/API_DB",        conn_max_age=600)
+        default="postgresql://postgres:postgres@localhost:5432/APIDB",        conn_max_age=600)
 }
