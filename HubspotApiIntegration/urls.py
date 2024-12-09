8013ab8536883d8eb73e6ffaa44f2edaebe1d516
Boualam3 <boualam.bourahla@gmail.com>
2024-12-09 15:06:44 +0100

    add health checker

--- HubspotApiIntegration/urls.py
+++ HubspotApiIntegration/urls.py
@@ -1,11 +1,17 @@
 """
 URL configuration for HubspotApiIntegration project.
 """
-
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import include, path
 
+
+def health_check(request):
+    return HttpResponse("OK", content_type="text/plain")
+
+
 urlpatterns = [
+    path("/", health_check),
     path("admin/", admin.site.urls),
     path("api/auth/", include("authapp.urls")),
     path("api/crm/", include("crm_integration.urls")),
