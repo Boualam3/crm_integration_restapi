f53e268c3b8e4f58de8e5781686ce14d23f6ee2c
Boualam3 <boualam.bourahla@gmail.com>
2024-12-09 01:44:05 +0100

    update prod

--- HubspotApiIntegration/settings/prod.py
+++ HubspotApiIntegration/settings/prod.py
@@ -1,8 +1,32 @@
 
 import dj_database_url
 
+DEBUG = False
+
+ALLOWED_HOSTS=["https://restapi-yzh1.onrender.com"]
+
+
+INSTALLED_APPS += ['corsheaders']
+
+MIDDLEWARE += [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+]
+
+
 
 DATABASES = {
     'default': dj_database_url.config(
         default="postgresql://postgres:postgres@localhost:5432/api_db",        conn_max_age=600)
 }
+
